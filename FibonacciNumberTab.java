45de27a7aec7f1e58cd35e26b83f242b9197f695
shivani gupta <shivanigupta4673@gmail.com>
2023-01-03 10:01:21 +0530

    Problem_23

---
+++ FibonacciNumberTab.java
@@ -0,0 +1,21 @@
+package com.dynamicProgramming;
+
+public class FibonacciNumberTab {
+	public static int fibTab(int n)
+	{
+		int dp[]=new int [n+1];
+		dp[0]=0;
+		dp[1]=1;
+		for(int i=2;i<n+1;i++) {
+			dp[i]=dp[i-1]+dp[i-2];
+		}
+		return dp[n];
+		
+	}
+	public static void main(String[] args) {
+		int n=5;
+		System.out.println(fibTab(n));
+
+	}
+
+}
